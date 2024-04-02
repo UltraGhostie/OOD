2d2a96c217f6339ff83e2b806a106e85b916fded
UltraGhostie <38960155+UltraGhostie@users.noreply.github.com>
2024-04-02 17:12:00 +0200

    1.0.1
    Removed unnecessary View functions and tests

--- pos/src/main/java/se/kth/iv1350/view/View.java
+++ pos/src/main/java/se/kth/iv1350/view/View.java
@@ -26,49 +26,6 @@ public class View {
         this.controller = controller;
     }
 
-    /**
-     * Starts a new sale.
-     */
-    public void startSale()
-    {
-        saleInfo = controller.startSale();
-        outputSale();
-    }
-
-    /**
-     * Attempts to enter a new item into the current sale.
-     * @param itemID The unique id of the item to enter into the current sale.
-     */
-    public void scanItem(int itemID)
-    {
-        SaleDTO invalidItemID = null;
-
-        SaleDTO returnedInfo = controller.scanItem(itemID);
-        if (returnedInfo == invalidItemID) {
-            System.out.println("Invalid item id");
-            return;
-        }
-        saleInfo = returnedInfo;
-        outputSale();
-    }
-
-    /**
-     * Attempts to set the count of the item with id itemID to the given non-zero, non-negative count
-     * @param itemID The id of the item to be changed.
-     * @param itemCount The new count of the item.
-     */
-    public void setCount(int itemID, int itemCount)
-    {
-        SaleDTO error = null;
-
-        SaleDTO returnedInfo = controller.setCount(itemID, itemCount);
-        if (saleInfo == error) {
-            System.out.println("Invalid id or count");
-            return;
-        }
-        saleInfo = returnedInfo;
-        outputSale();
-    }
 
     private void outputSale()
     {
--- pos/src/test/java/se/kth/iv1350/view/ViewTest.java
+++ pos/src/test/java/se/kth/iv1350/view/ViewTest.java
@@ -1,17 +1,7 @@
 package se.kth.iv1350.view;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.io.PrintStream;
-import java.util.Scanner;
-
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import se.kth.iv1350.controller.Controller;
 import se.kth.iv1350.integration.Integration;
@@ -21,23 +11,10 @@ import se.kth.iv1350.integration.Integration;
  */
 public class ViewTest {
     static Controller controller;
-    static PrintStream standard;
-    static File outFile;
-    static PrintStream out;
     static View view;
 
     /**
-     * Sets standard output to a temp file used for testing.
-     */
-    @BeforeClass
-    public static void initClass()
-    {
-        outFile = new File("asdhbfahsdbguaksdg.txt");
-        standard = System.out;
-    }
-
-    /**
-     * Initializes temporary variables and creates the testing file.
+     * Initializes temporary variables.
      */
     @Before
     public void init()
@@ -45,155 +22,15 @@ public class ViewTest {
         controller = new Controller(new Integration());
         controller.startSale();
         view = new View(controller);
-        view.startSale();
-
-        try {
-            outFile.createNewFile();
-            out = new PrintStream(outFile);
-            System.setOut(out);
-        } catch (Exception e) {
-            System.out.println("Could not create file test.txt, aborting test");
-            System.exit(0);
-        }
     }
 
     /**
-     * Empties the temporary variables and removes the temp file.
+     * Empties the temporary variables.
      */
     @After
     public void clean()
     {
         view = null;
         controller = null;
-        System.setOut(standard);
-        if (out != null) {
-            out.close();
-        }
-        System.gc();
-        outFile.delete();
-    }
-
-    /**
-     * Checks that startSale() runs.
-     */
-    @Test
-    public void startSaleTest()
-    {
-        view.startSale();
-        assertTrue(true);
-    }
-
-    /**
-     * Checks that scanning a valid item adds it correctly.
-     */
-    @Test
-    public void scanValidItemTest()
-    {
-        int validID = 1;
-        String expected = "Cost: 1.0";
-
-        try {
-            view.scanItem(validID);
-        } catch (Exception e) {
-            System.err.println(e);
-            fail();
-        }
-
-        try {
-            Scanner scanner = new Scanner(outFile);
-            String fileContents = readTestFile(scanner);
-            assertLastLineEquals(expected, fileContents);
-        } catch (Exception e) {
-            System.err.println(e);
-            fail();
-        }
-    }
-
-    /**
-     * Checks that scanning an invalid item produces the expected output.
-     */
-    @Test
-    public void scanInvalidItemTest()
-    {
-        int invalidID = -1234;
-        String invalidIDMessage = "Invalid item id";
-
-        view.scanItem(invalidID);
-
-        try {
-            Scanner scanner = new Scanner(outFile);
-            String fileContents = readTestFile(scanner);
-            assertLastLineEquals(invalidIDMessage, fileContents);
-        } catch (Exception e) {
-            System.err.println(e);
-            fail();
-        }
-    }
-
-    /**
-     * Testing that scanning an item multiple times creates the expected output.
-     */
-    @Test
-    public void scanItemMultipleTimesTest()
-    {
-        int validID = 1;
-        int times = 3;
-
-
-        for (int i = 0; i < times; i++) {
-            view.scanItem(validID);
-        }
-
-        assertItemCount(times);
-    }
-
-    /**
-     * Checks that setting the count of an item produces the expected output.
-     */
-    @Test
-    public void setValidCountTest()
-    {
-        int validID = 1;
-        int newCount = 5;
-
-        view.scanItem(validID);
-        view.setCount(validID, newCount);
-        assertItemCount(newCount);
-    }
-
-    private void assertItemCount(int expectedCount)
-    {
-        try {
-            Scanner scanner = new Scanner(outFile);
-            String output = readTestFile(scanner);
-            String[] split = output.split("\\r\\n\\r\\n");
-            output = split[split.length-1];
-            split = output.split("\\r\\n");
-
-            for (String string : split) {
-                if (string.contains("*")) {
-                    Integer count = Integer.parseInt(string.split("*")[1].split(",")[0]);
-                    assertEquals(expectedCount, (int)count);
-                }
-            }
-        } catch (Exception e) {
-            fail();
-        }
-    }
-
-    private String readTestFile(Scanner scanner)
-    {
-        String fileContents = new String();
-        while (scanner.hasNextLine()) {
-            fileContents += "\n"+scanner.nextLine();
-        }
-        return fileContents;
-    }
-
-    private void assertLastLineEquals(String expected, String actual)
-    {
-        String[] split = actual.trim().split("\\n");
-        String lastString = split[split.length-1];
-        assertEquals(expected.trim(), lastString.trim());
     }
 }
